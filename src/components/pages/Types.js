a5ac44d506604b0266158157c5a8fa29339e859d
Joel <sippun@users.noreply.github.com>
2022-05-09 15:23:52 -0700

    Fix Types page hyphens

--- src/components/pages/Types.js
+++ src/components/pages/Types.js
@@ -6,21 +6,21 @@ export default function About() {
       <br/><br/><br/><br/>
           {/* pre tag doesn't work here :( */}
           <p>EMTJ - Itachi Uchiha</p>
-          <p>EMTS – Joseph Joestar?</p>
-          <p>EMRJ – Kyojuro Rengoku, Tsunade, Gaara</p>
-          <p>EMRS – Hinata Hyuga</p>
-          <p>EPTJ – Kaneki Ken</p>
-          <p>EPTS – Ippo Makunouchi</p>
-          <p>EPRJ – Takemichi Hanagaki, Eren Yeager?, Nezuko Kamado?</p>
-          <p>EPRS – Rock Lee, Might Guy, Luffy, Saitama, Sakura Haruno</p>
-          <p>IMTJ – Shikamaru Nara, Ochaco Uraraka</p>
-          <p>IMTS – Kakashi Hatake, Shinobu Kocho, Armin</p>
-          <p>IMRJ – Tanjiro Kamado, Ichigo Kurosaki, Usagi Tsukino/Sailor Moon?, FSN Saber?</p>
-          <p>IMRS – Shigeo Kageyama (Mob), Zenitsu Agatsuma, Sasuke Uchiha</p>
-          <p>IPTJ – Izuku Midoriya, Mikasa Ackerman?, Motoko Kusanagi?</p>
-          <p>IPTS – Aoi Todo</p>
-          <p>IPRJ – Yuji Itadori, Whitebeard?</p>
-          <p>IPRS – Naruto Uzumaki, Katsuki Bakugo</p>
+          <p>EMTS - Joseph Joestar?</p>
+          <p>EMRJ - Kyojuro Rengoku, Tsunade, Gaara</p>
+          <p>EMRS - Hinata Hyuga</p>
+          <p>EPTJ - Kaneki Ken</p>
+          <p>EPTS - Ippo Makunouchi</p>
+          <p>EPRJ - Takemichi Hanagaki, Eren Yeager?, Nezuko Kamado?</p>
+          <p>EPRS - Rock Lee, Might Guy, Luffy, Saitama, Sakura Haruno</p>
+          <p>IMTJ - Shikamaru Nara, Ochaco Uraraka</p>
+          <p>IMTS - Kakashi Hatake, Shinobu Kocho, Armin</p>
+          <p>IMRJ - Tanjiro Kamado, Ichigo Kurosaki, Usagi Tsukino/Sailor Moon?, FSN Saber?</p>
+          <p>IMRS - Shigeo Kageyama (Mob), Zenitsu Agatsuma, Sasuke Uchiha</p>
+          <p>IPTJ - Izuku Midoriya, Mikasa Ackerman?, Motoko Kusanagi?</p>
+          <p>IPTS - Aoi Todo</p>
+          <p>IPRJ - Yuji Itadori, Whitebeard?</p>
+          <p>IPRS - Naruto Uzumaki, Katsuki Bakugo</p>
     </>
   );
 }
