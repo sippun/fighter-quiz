a73a7753ac47300a35a6674ae54a98a9827c4707
Joel <sippun@users.noreply.github.com>
2022-05-09 17:17:24 -0700

    Add show/hide cards on About page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import './About.css';
 
 export default function About() {
+  const EITYPE = 'EI', PMTYPE = 'PM', TRTYPE = 'TR', JSTYPE = 'JS';
+  const [showEI, setShowEI] = useState(false);
+  const [showPM, setShowPM] = useState(false);
+  const [showTR, setShowTR] = useState(false);
+  const [showJS, setShowJS] = useState(false);
+
+  const handleClick = (type) => {
+    switch (type) {
+      case EITYPE: setShowEI(!showEI);
+      break;
+      case PMTYPE: setShowPM(!showPM);
+      break;
+      case TRTYPE: setShowTR(!showTR);
+      break;
+      case JSTYPE: setShowJS(!showJS);
+      break;
+    }
+  }
+
   return (
-    <>
+    <div className="about-container">
       <br/><br/><br/><br/>
         <p>
           The global organization responsible for developing combatants in this world,
@@ -10,21 +30,45 @@ export default function About() {
           to type fighters as follows:
         </p>
 
-        <h1>Enduring vs. Intensive</h1>
-        <p>Enduring: outlast, wear down, tire out opponent</p>
-        <p>Intensive: decisive strikes, explosiveness</p>
-
-        <h1>Mental vs. Physical</h1>
-        <p>Mental: outwit, out plan, make reads, and mind game</p>
-        <p>Physical: faster, stronger, or other physical advantage</p>
-
-        <h1>Tactical vs. Reflexive</h1>
-        <p>Tactical: study opponent, plan countermeasures, set traps</p>
-        <p>Reflexive: instinctive counters, strategize on the fly</p>
-
-        <h1>Justice vs. Self</h1>
-        <p>Justice: Reasons for fighting come from belief in principles or protecting others</p>
-        <p>Self: Motivated by personal reasons</p>
-    </>
+        <div className="desc-container" onClick={() => handleClick(EITYPE)}>
+          <h1>Enduring vs. Intensive</h1>
+          { showEI ? 
+            <div className="desc-content">
+              <p>Enduring: outlast, wear down, tire out opponent</p>
+              <p>Intensive: decisive strikes, explosiveness</p>
+            </div>
+          : null }
+        </div>
+        
+        <div className="desc-container" onClick={() => handleClick(PMTYPE)}>
+          <h1>Mental vs. Physical</h1>
+          { showPM ? 
+            <div className="desc-content">
+              <p>Mental: outwit, out plan, make reads, and mind game</p>
+              <p>Physical: faster, stronger, or other physical advantage</p>
+            </div>
+          : null }
+        </div>
+        
+        <div className="desc-container" onClick={() => handleClick(TRTYPE)}>
+          <h1>Tactical vs. Reflexive</h1>
+          { showTR ? 
+            <div className="desc-content">
+              <p>Tactical: study opponent, plan countermeasures, set traps</p>
+              <p>Reflexive: instinctive counters, strategize on the fly</p>
+            </div>
+          : null }
+        </div>
+        
+        <div className="desc-container" onClick={() => handleClick(JSTYPE)}>
+          <h1>Justice vs. Self</h1>
+          { showJS ? 
+            <div className="desc-content">
+              <p>Justice: Reasons for fighting come from belief in principles or protecting others</p>
+              <p>Self: Motivated by personal reasons</p>
+          </div>
+          : null }
+        </div>
+    </div>
   );
 }
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -5,12 +5,14 @@ export default function Home() {
   return (
     <div className="home-container">
       <br/><br/><br/>
-      <div className="text-wrapper">
-        <h1 className="home-text">You wake up one day in a strange world, finding it filled with all kinds of combat styles</h1>
-        <h2 className="home-text">To determine the kind of potential you have in this world, this system will administer a test to determine your archetype</h2>
-        <h2 className="home-text">What kind of fighter will you become?</h2>
-      </div>
+      <div className="home-content">
+        <div className="text-wrapper">
+          <h1 className="home-text">You wake up one day in a strange world, finding it filled with all kinds of combat styles</h1>
+          <h2 className="home-text">To determine the kind of potential you have in this world, this system will administer a test to determine your archetype</h2>
+          <h2 className="home-text">What kind of fighter will you become?</h2>
+        </div>
         <Link to="/quiz" className="button-link">Take the quiz</Link>
+      </div>
     </div>
   );
 }
