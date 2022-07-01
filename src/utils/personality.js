8c23ab8a2d27d0ec721f211efd162869e3fd4f07
Joel <sippun@users.noreply.github.com>
2022-06-30 22:23:49 -0700

    Create personality.js

---
+++ src/utils/personality.js
@@ -0,0 +1,12 @@
+
+export const fpscales = {
+  TYPE1: 'EI',
+  TYPE2: 'PM',
+  TYPE3: 'TR',
+  TYPE4: 'JS',
+};
+
+export const fptypes = [
+  'EMTJ', 'EMTS', 'EMRJ', 'EMRS', 'EPTJ', 'EPTS', 'EPRJ', 'EPRS',
+  'IMTJ', 'IMTS', 'IMRJ', 'IMRS', 'IPTJ', 'IPTS', 'IPRJ', 'IPRS',
+];
