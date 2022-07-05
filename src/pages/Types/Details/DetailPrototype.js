3d9e41948338ca307a2446d9387cbd2f657c4dbf
Joel <sippun@users.noreply.github.com>
2022-07-05 16:03:11 -0700

    Add return link to DetailPrototype

--- src/pages/Types/Details/DetailPrototype.js
+++ src/pages/Types/Details/DetailPrototype.js
@@ -1,10 +1,14 @@
 import { fptypes } from "../../../utils/personality";
 
+import { Link } from 'react-router-dom';
+
 export default function DetailPrototype() {
   const type = fptypes[0];
   return (
     <div>
-      <br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/>
+      <Link to="/types">Back to all types</Link>
+      <br/><br/>
       <p>
         {type}
       </p>
