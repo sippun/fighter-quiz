bf6a2051e6bf326e4c88dae400ff3f46b9c68f31
Joel <sippun@users.noreply.github.com>
2022-07-06 16:35:01 -0700

    Add axios and use it to get initial data from Jikan api

--- src/pages/Types/DetailPage.js
+++ src/pages/Types/DetailPage.js
@@ -1,9 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getType } from './details';
 
 function DetailPage() {
+  const [charInfo, setCharInfo] = useState(null);
   const { type } = useParams();
   const typeObj = getType(type);
+  const baseURL = "https://api.jikan.moe/v4/characters/14/full"
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setCharInfo(response.data);
+    });
+  }, []);
+
+  if (!charInfo) return null;
 
   return (
     <div>
@@ -11,7 +23,10 @@ function DetailPage() {
       <p>{typeObj.description}</p>
       <ul>
         {typeObj.characters.map(({ name }) => (
+          <>
           <li>{name}</li>
+          <p>{charInfo.data.url}</p>
+          </>
         ))}
 
       </ul>
