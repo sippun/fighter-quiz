b671d46602e20a5ce2e298d891ad18a7026a3cda
Joel <sippun@users.noreply.github.com>
2022-07-06 15:09:10 -0700

    Access type description in DetailPage

--- src/pages/Types/DetailPage.js
+++ src/pages/Types/DetailPage.js
@@ -3,11 +3,12 @@ import { getType } from './details';
 
 function DetailPage() {
   const { type } = useParams();
+  const typeObj = getType(type);
 
   return (
     <div>
       <h1>{type}</h1>
-
+      <p>{typeObj.description}</p>
       <hr />
     </div>
   );
