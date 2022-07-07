dca279539fbe2ec5cc72b7a3bcb05e38c83fdd7c
Joel <sippun@users.noreply.github.com>
2022-07-06 17:22:12 -0700

    Add all types to details array
    Still need to fill out mal ids

--- src/pages/Types/details.js
+++ src/pages/Types/details.js
@@ -81,6 +81,188 @@ const details = [
       }
     ],
   },
+  {
+    name: "EPTS",
+    description: "Enduring, Physical, Tactical, Self",
+    characters: [
+      {
+        name: "Ippo Makunouchi",
+        mal_id: "",
+      }
+    ],
+  },
+  {
+    name: "EPRJ",
+    description: "Enduring, Physical, Reflexive, Justice",
+    characters: [
+      {
+        name: "Takemichi Hanagaki",
+        mal_id: "",
+      },
+      {
+        name: "Eren Yeager",
+        mal_id: "",
+      },
+      {
+        name: "Nezuko Kamado",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "EPRS",
+    description: "Enduring, Physical, Reflexive, Self",
+    characters: [
+      {
+        name: "Rock Lee",
+        mal_id: "",
+      },
+      {
+        name: "Might Guy",
+        mal_id: "",
+      },
+      {
+        name: "Luffy",
+        mal_id: "",
+      },
+      {
+        name: "Saitama",
+        mal_id: "",
+      },
+      {
+        name: "Sakura Haruno",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IMTJ",
+    description: "Intensive, Mental, Tactical, Justice",
+    characters: [
+      {
+        name: "Shikamaru Nara",
+        mal_id: "",
+      },
+      {
+        name: "Ochako Uraraka",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IMTS",
+    description: "Intensive, Mental, Tactical, Self",
+    characters: [
+      {
+        name: "Kakashi Hatake",
+        mal_id: "",
+      },
+      {
+        name: "Shinobu Kocho",
+        mal_id: "",
+      },
+      {
+        name: "Armin",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IMRJ",
+    description: "Intensive, Mental, Reflexive, Justice",
+    characters: [
+      {
+        name: "Tanjiro Kamado",
+        mal_id: "",
+      },
+      {
+        name: "Ichigo Kurosaki",
+        mal_id: "",
+      },
+      {
+        name: "Usagi Tsukino",
+        mal_id: "",
+      },
+      {
+        name: "Saber",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IMRS",
+    description: "Intensive, Mental, Reflexive, Self",
+    characters: [
+      {
+        name: "Shigeo Kageyama",
+        mal_id: "",
+      },
+      {
+        name: "Zenitsu Agatsuma",
+        mal_id: "",
+      },
+      {
+        name: "Sasuke Uchiha",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IPTJ",
+    description: "Intensive, Physical, Tactical, Justice",
+    characters: [
+      {
+        name: "Izuku Midoriya",
+        mal_id: "",
+      },
+      {
+        name: "Mikasa Ackerman",
+        mal_id: "",
+      },
+      {
+        name: "Motoko Kusanagi",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IPTS",
+    description: "Intensive, Physical, Tactical, Self",
+    characters: [
+      {
+        name: "Aoi Todo",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IPRJ",
+    description: "Intensive, Physical, Reflexive, Justice",
+    characters: [
+      {
+        name: "Yuji Itadori",
+        mal_id: "",
+      },
+      {
+        name: "Whitebeard",
+        mal_id: "",
+      },
+    ],
+  },
+  {
+    name: "IPRS",
+    description: "Intensive, Physical, Reflexive, Self",
+    characters: [
+      {
+        name: "Naruto Uzumaki",
+        mal_id: "",
+      },
+      {
+        name: "Katsuki Bakugo",
+        mal_id: "",
+      },
+    ],
+  },
 ];
 
 export function getType(typeId) {
